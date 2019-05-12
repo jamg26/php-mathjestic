6272fb9fddd50a5fe595f2c7c0e2bbf2558ba775
Jamuel <jammmg26@gmail.com>
2019-05-12 18:57:21 +0800

    reverting db to localhost

--- database/admin_fol/dbconn.php
+++ database/admin_fol/dbconn.php
@@ -1,3 +1,3 @@
 <?php
-	$conn=mysqli_connect("13.250.121.140","root","", "pg6cv1-database") or die(mysql_error());
+	$conn=mysqli_connect("localhost","root","", "pg6cv1-database") or die(mysql_error());
 ?>
